007e5f650722f17eb05c302eb05dfab28adecd43
YongWoonJi <woon013@naver.com>
2016-08-19 19:54:50 +0900

    Initial commit

---
+++ app/src/main/java/com/example/tacademy/samplefacebook/MainActivity.java
@@ -0,0 +1,134 @@
+package com.example.tacademy.samplefacebook;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.facebook.AccessToken;
+import com.facebook.AccessTokenTracker;
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.login.DefaultAudience;
+import com.facebook.login.LoginBehavior;
+import com.facebook.login.LoginManager;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
+
+import java.util.Arrays;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    LoginButton loginButton;
+    CallbackManager callbackManager;
+    LoginManager mLoginManager;
+    Button facebookButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        callbackManager = CallbackManager.Factory.create();
+        mLoginManager = LoginManager.getInstance();
+        loginButton = (LoginButton) findViewById(R.id.login_button);
+        loginButton.setReadPermissions("email");
+        loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                Toast.makeText(MainActivity.this, "facebook login success", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onCancel() {
+
+            }
+
+            @Override
+            public void onError(FacebookException error) {
+
+            }
+        });
+
+        facebookButton = (Button) findViewById(R.id.btn_login);
+        facebookButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (isLogin()) {
+                    logoutFacebook();
+                } else {
+                    loginFaceBook();
+                }
+            }
+        });
+
+        setButtonLabel();
+
+    }
+
+    private void setButtonLabel() {
+        if (isLogin()) {
+            facebookButton.setText("logout");
+        } else {
+            facebookButton.setText("login");
+        }
+    }
+
+    AccessTokenTracker mTracker;
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        if (mTracker == null) {
+            mTracker = new AccessTokenTracker() {
+                @Override
+                protected void onCurrentAccessTokenChanged(AccessToken oldAccessToken, AccessToken currentAccessToken) {
+                    setButtonLabel();
+                }
+            };
+        } else {
+            mTracker.startTracking();
+        }
+    }
+
+    private void logoutFacebook() {
+        mLoginManager.logOut();
+    }
+
+    private void loginFaceBook() {
+        mLoginManager.setDefaultAudience(DefaultAudience.FRIENDS);
+        mLoginManager.setLoginBehavior(LoginBehavior.NATIVE_WITH_FALLBACK);
+        mLoginManager.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                Toast.makeText(MainActivity.this, "login manager", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onCancel() {
+
+            }
+
+            @Override
+            public void onError(FacebookException error) {
+
+            }
+        });
+        mLoginManager.logInWithReadPermissions(this, Arrays.asList("email"));
+    }
+
+    private boolean isLogin() {
+        AccessToken token = AccessToken.getCurrentAccessToken();
+        return token != null;
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        callbackManager.onActivityResult(requestCode, resultCode, data);
+    }
+}
